4caff5814fb79e63bb45af2ff49475d12b72ee62
Sahil-4 <www.sahil.875020@gmail.com>
2024-03-25 22:24:48 +0530

    added function to generate otp

--- Utils/index.js
+++ Utils/index.js
@@ -1,6 +1,7 @@
 // function to generate a random index 0 to 62
 const getRandomIndex = () => Math.floor(Math.random() * 62);
 
+// function to generate a random hash of 6 length for short url
 const getHash = () => {
   const characterSet = "A012BC3456bcdefhijkQRSTnoHIJKLpqUVlmrstDWXYZagEFwxyNOGMPuvz789";
   let hash = "";
@@ -8,9 +9,18 @@ const getHash = () => {
   for (let i = 0; i < 7; i++) hash += characterSet[getRandomIndex()];
 
   return hash;
-}; 
+};
 
 // function to generate short URL
 export const getShortURL = () => {
   return `${process.env.DOMAIN}/${getHash()}`;
+};
+
+// function to generate string of 6 numbers for otp 
+export const generateOTP = () => {
+  let otp = "";
+
+  for (let i = 0; i < 6; i++) otp += Math.floor(Math.random() * 10);
+
+  return otp;
 };
